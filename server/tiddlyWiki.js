bb2cf1c0976f5f3087fd10a4d7a4f5cbfb6b38c7
Jim Pick <jim@backblend.com>
2018-05-11 21:15:07 -0700

    Get tiddlywiki crudely working

--- server/tiddlyWiki.js
+++ server/tiddlyWiki.js
@@ -5,18 +5,31 @@ const tiddlywikiBoot = require('tiddlywiki/boot/boot')
 $tw = tiddlywikiBoot.TiddlyWiki()
 $tw.boot.argv = ['tiddlywikiBase']
 $tw.boot.boot()
+$tw.wiki.addTiddler({
+  "text": "$protocol$//$host$/doc/20949bc4db2ac0ed80ebb20bbb2b79114c9dee65f511657a87678a865f39617f/tw/",
+	"title": "$:/config/tiddlyweb/host"
+})
 const serverCommand = $tw.modules.execute('tiddlywiki/core/modules/commands/server.js').Command
 const command = new serverCommand([], {wiki: $tw.wiki})
 const server = command.server
 const requestHandler = server.requestHandler.bind(server)
 
-function tiddlyWikiRequest (req, res, next) {
+function tiddlyWikiRequest (req, res) {
   console.log('Jim tiddlyWikiRequest', req.url)
   server.set({
+    rootTiddler: "$:/core/save/all",
+		renderType: "text/plain",
+		serveType: "text/html",
+		username: "",
+		password: "",
     pathPrefix: req.url.replace(/\/tw.*$/, '')
   })
-  req.url = '/'
+  req.url = req.url.replace(/^.*\/tw\/?/, '/')
   requestHandler(req, res, next)
+
+  function next () {
+    console.log('Jim next', req.url)
+  }
 }
 
 module.exports = tiddlyWikiRequest
