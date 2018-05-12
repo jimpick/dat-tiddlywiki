fd2ec1f9891334385abab0aae754aa27c0436fcb
Jim Pick <jim@backblend.com>
2018-05-11 22:43:39 -0700

    Build up a cache of tiddlywiki instances
    Indexed by key

--- server/tiddlyWiki.js
+++ server/tiddlyWiki.js
@@ -2,34 +2,43 @@ const tiddlywikiBoot = require('tiddlywiki/boot/boot')
 
 // See: https://gist.github.com/Arlen22/bbd852f68e328165e49f
 
-$tw = tiddlywikiBoot.TiddlyWiki()
-$tw.boot.argv = ['tiddlywikiBase']
-$tw.boot.boot()
-$tw.wiki.addTiddler({
-  "text": "$protocol$//$host$/doc/20949bc4db2ac0ed80ebb20bbb2b79114c9dee65f511657a87678a865f39617f/tw/",
-	"title": "$:/config/tiddlyweb/host"
-})
-const serverCommand = $tw.modules.execute('tiddlywiki/core/modules/commands/server.js').Command
-const command = new serverCommand([], {wiki: $tw.wiki})
-const server = command.server
-const requestHandler = server.requestHandler.bind(server)
+const wikis = {}
 
-function tiddlyWikiRequest (req, res) {
-  console.log('Jim tiddlyWikiRequest', req.url)
+function getWiki(key) {
+  if (wikis[key]) return wikis[key]
+  $tw = tiddlywikiBoot.TiddlyWiki()
+  $tw.boot.argv = ['tiddlywikiBase']
+  $tw.boot.boot()
+  $tw.wiki.addTiddler({
+    text: `$protocol$//$host$/doc/${key}/tw/`,
+    title: '$:/config/tiddlyweb/host'
+  })
+  const serverCommand = $tw.modules.execute(
+    'tiddlywiki/core/modules/commands/server.js'
+  ).Command
+  const command = new serverCommand([], {wiki: $tw.wiki})
+  const server = command.server
   server.set({
     rootTiddler: "$:/core/save/all",
-		renderType: "text/plain",
-		serveType: "text/html",
-		username: "",
-		password: "",
-    pathPrefix: req.url.replace(/\/tw.*$/, '')
+    renderType: "text/plain",
+    serveType: "text/html",
+    username: "",
+    password: "",
+    pathPrefix: `/doc/${key}/tw/`
   })
-  req.url = req.url.replace(/^.*\/tw\/?/, '/')
-  requestHandler(req, res, next)
-
-  function next () {
-    console.log('Jim next', req.url)
+  const requestHandler = server.requestHandler.bind(server)
+  wikis[key] = {
+    command,
+    requestHandler
   }
+  return wikis[key]
+}
+
+function tiddlyWikiRequest (req, res, next) {
+  console.log('Jim tiddlyWikiRequest', req.url, req.params)
+  const wiki = getWiki(req.params.key)
+  req.url = req.url.replace(/^.*\/tw\/?/, '/')
+  wiki.requestHandler(req, res, next)
 }
 
 module.exports = tiddlyWikiRequest
