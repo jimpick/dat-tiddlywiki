28a5d4d8e00b5f10bea08ff84c335642a4066188
Glitch (dat-tiddlywiki-dev) <none>
2018-05-12 03:35:30 +0000

    👨‍👨‍👦💎 Updated with Glitch 2

--- server/tiddlyWiki.js
+++ server/tiddlyWiki.js
@@ -1,7 +1,22 @@
-module.exports = tiddlyWiki
+const tiddlywikiBoot = require('tiddlywiki/boot/boot')
 
-function tiddlyWiki (req, res, next) {
-  console.log('Jim tw', req.url)
-  res.write('TiddlyWiki')
-  res.end()
+// See: https://gist.github.com/Arlen22/bbd852f68e328165e49f
+
+$tw = tiddlywikiBoot.TiddlyWiki()
+$tw.boot.argv = ['tiddlywikiBase']
+$tw.boot.boot()
+const serverCommand = $tw.modules.execute('tiddlywiki/core/modules/commands/server.js').Command
+const command = new serverCommand([], {wiki: $tw.wiki})
+const server = command.server
+const requestHandler = server.requestHandler.bind(server)
+
+function tiddlyWikiRequest (req, res, next) {
+  console.log('Jim tiddlyWikiRequest', req.url)
+  server.set({
+    pathPrefix: req.url.replace(/\/tw.*$/, '')
+  })
+  req.url = '/'
+  requestHandler(req, res, next)
 }
+
+module.exports = tiddlyWikiRequest
